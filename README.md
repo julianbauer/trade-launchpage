9d9a7e3a4a60ce13ac88aa09e75bf638c73d9b42
julianbauer <julian@overnice.de>
2016-01-28 22:21:22 +0100

    restructured+

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # trade-launchpage
+
+[![wercker status](https://app.wercker.com/status/9e9dfc7e043db2eefca18a713674cc18/s "wercker status")](https://app.wercker.com/project/bykey/9e9dfc7e043db2eefca18a713674cc18)
--- app/components/Html/index.jsx
+++ app/components/Html/index.jsx
@@ -1,17 +1,51 @@
 import './index.styl';
-import Start from '../Start/index.jsx';
 import React from 'react';
+import classNames from 'classnames';
+// import scrollTo from 'scroll-to';
 
-
-// COMPONENT MERGE
-// ---------------
+// Base Component
 
 export default class Html extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state = ({ isImprintVisible: false });
+		this.handleClose = this.handleClose.bind(this);
+		this.handleOpen = this.handleOpen.bind(this);
+	}
+
+	handleOpen() {
+		this.setState({ isImprintVisible: true });
+	}
+
+	handleClose() {
+		this.setState({ isImprintVisible: false });
+	}
+
 	render() {
+
+		const cx = classNames({ imprintIsVisible: this.state.isImprintVisible });
+
 		return (
-			<div>
-				<Start />
+			<div id="page" className={cx}>
+				<section id="start">
+					<div id="logo"></div>
+				</section>
+				<section id="impressum">
+					<div id="closeImpressum" onClick={this.handleClose}></div>
+					<h2>Responsible for this website:</h2>
+					<p>
+						tradeapp UG iGr<br/>
+						Gerichtstr. 18<br/>
+						13347 Berlin
+					</p>
+					<p className="light">
+						Geschäftsführer: <br/>
+						Julian Bauer,<br/>
+						Tobias Weinmann
+					</p>
+				</section>
+				<div id="linkToImpressum" onClick={this.handleOpen}>Imprint</div>
 			</div>
 		)
 	}
